45cf326590734ef09ea89e53481c748e0e1e2b12
Sameet Sapra <sameetandpotatoes@gmail.com>
2017-02-21 14:21:16 -0600

    Redo bs4 example

--- scraper.py
+++ scraper.py
@@ -1,13 +1,24 @@
 from bs4 import BeautifulSoup
+from bs4 import UnicodeDammit
+
 import urllib
-r = urllib.urlopen('https://www.reddit.com/').read()
-soup = BeautifulSoup(r, "html.parser")
-#print type(soup)
-#print soup
-#print soup.prettify()
-#print soup.text
+import requests
+import IPython
+
+r = requests.get("http://www.cnn.com/2017/02/21/politics/donald-trump-republican-support/index.html")
+soup = BeautifulSoup(r.text, "html.parser")
 
 
+print "CNN Article:"
 
-#content = soup.find_all("div", class="content")
-#print content
+print "Title:"
+article_title = soup.find("h1", {"class": "pg-headline"})
+print article_title.text
+
+print "Update Time:"
+article_time = soup.find("p", {"class": "update-time"})
+print article_time.text
+
+print "Content:"
+article_content = soup.find("section", {"class": "zn-body-text"})
+print article_content.text
